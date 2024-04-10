db6fe530e94ff1d56cefce0858701942902708a2
Liuchengqing <410150249@qq.com>
2024-04-10 17:30:29 +0800

    master 1c

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+/**
+ * @Author :l
+ * @Create :2024/4/10 17:25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!git idea demo");
+    }
+}
